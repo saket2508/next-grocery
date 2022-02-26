08ebb1daa69dfb552cbae25d7ed1446491f27fd4
saket2508 <saketns@gmail.com>
2022-02-26 10:13:19 +0530

    fix deploy issue errors

--- pages/login.tsx
+++ pages/login.tsx
@@ -194,7 +194,7 @@ const Login: NextPage = () => {
                                                             cy="12"
                                                             r="10"
                                                             stroke="currentColor"
-                                                            stroke-width="4"
+                                                            strokeWidth="4"
                                                         ></circle>
                                                         <path
                                                             className="opacity-75"
--- pages/register.tsx
+++ pages/register.tsx
@@ -243,7 +243,7 @@ const Register: NextPage = () => {
                                                             cy="12"
                                                             r="10"
                                                             stroke="currentColor"
-                                                            stroke-width="4"
+                                                            strokeWidth="4"
                                                         ></circle>
                                                         <path
                                                             className="opacity-75"
